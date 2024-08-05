0d446a53a05cea4843b1c594c321634a8fc12f25
G-irish <123972157+G-irish@users.noreply.github.com>
2024-08-05 18:20:48 +0530

    second commit

--- Server.js
+++ Server.js
@@ -13,6 +13,10 @@ connectDB();
 app.use('/api/files',require('./Routes/files'));
 app.use('/files',require('./Routes/Show'));
 app.use('/files/download',require('./Routes/download'));
+app.use(express.static(path.join(__dirname,"/public")))
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/index.html"));
+})
 app.listen(PORT,()=>{
     console.log(`Server Running on ${PORT}`);
 })
